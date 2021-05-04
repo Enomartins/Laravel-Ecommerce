b3c76c16c93c69dc9973b173f4b53ae133217ca8
Enomartins <enowebservices@gmail.com>
2021-05-04 13:00:17 +0100

    all routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Read file route
+// Route::get('movies/{filename}', [StorageFileController::class,'getPubliclyStorgeFile'])->name('movies.displayImage');
+
+Route::get('/', 'MovieController@index')->name('welcome');
+Route::post('/movies', 'MovieController@store')->name('movie.store');
+Route::get('/movies/{id}', 'MovieController@show')->name('movie.show');
+Route::delete('/movies/{id}', 'MovieController@destroy')->name('movie.destroy');
+Route::post('/movies/search', 'MovieController@search')->name('movie.search');
+Route::get('/admin', 'MovieController@admin')->name('admin');
+
+
+Route::post('/profile', 'ProfileController@store')->name('profile.store');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/cart', 'CartController@index')->name('cart');
